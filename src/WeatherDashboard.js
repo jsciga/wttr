1d0e5d17566cde567d251301a47454c55b35b44f
Jakub Ściga <jakubsciga@Mac-mini-Jakub.local>
2024-12-07 14:57:31 +0100

    log

--- src/WeatherDashboard.js
+++ src/WeatherDashboard.js
@@ -14,6 +14,7 @@ const WeatherDashboard = () => {
         const response = await fetch('/api/v1/smog');
         
         if (!response.ok) {
+            console.log(response);
           throw new Error('Network response was not ok');
         }
         
